99d6338a426fabbe510ad8ad6c0c660f80fa59e5
jaiboy825 <jaiboy@naver.com>
2020-10-21 14:49:59 +0900

    10월 21일 js 수업끝

---
+++ pack/app.js
@@ -0,0 +1,8 @@
+import Vue from 'vue';
+import $ from 'jquery';
+
+console.log($);
+console.log(Vue);
+
+let a = 10;
+console.log(a);
---
+++ pack/webpack.config.js
@@ -0,0 +1,6 @@
+const path = require('path');
+module.exports = {
+    entry : path.join(__dirname, 'app.js'),
+    output : path.join(__dirname, 'dist/dist.js')
+}
+
